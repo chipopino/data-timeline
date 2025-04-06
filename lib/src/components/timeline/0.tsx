0d56cbfed268e20b080de699a15ac238c51de6b6
dannyGersh <dannygershman96@gmail.com>
2025-04-06 17:57:13 +0300

    clean up timeline

--- lib/src/components/timeline/0.tsx
+++ lib/src/components/timeline/0.tsx
@@ -1,37 +1,51 @@
 import React from "react";
 import { Timeline } from "./Timeline";
-import 'styles/root.css';
-import 'styles/theme.css';
+import "styles/root.css";
+import "styles/theme.css";
+import { differenceInDays } from "date-fns";
 
-import renderDev from 'shared/renderDev';
+import renderDev from "shared/renderDev";
 
 const mock = [
-    [
-        {
-            "title": "Declaration of the State of Israel",
-            "date": "1948-05-14",
-        },
-        {
-            "title": "made up event",
-            "date": "1948-05-13",
-        }
-    ],
-    [{
-        "title": "Six-Day War",
-        "date": "1967-06-05",
-    }],
-    [{
-        "title": "Yom Kippur War",
-        "date": "1973-10-06",
-    }],
-    [{
-        "title": "Israeli Withdrawal from Gaza",
-        "date": "2005-08-15",
-    }],
-    [{
-        "title": "Oslo Accords Signing",
-        "date": "1993-09-13",
-    }]
-]
+  {
+    title: "made up event",
+    date: "1948-05-13",
+  },
+  {
+    title: "Declaration of the State of Israel",
+    date: "1948-05-14",
+  },
+  {
+    title: "Six-Day War",
+    date: "1967-06-05",
+  },
+  {
+    title: "Yom Kippur War",
+    date: "1973-10-06",
+  },
+  {
+    title: "Oslo Accords Signing",
+    date: "1993-09-13",
+  },
+  {
+    title: "Israeli Withdrawal from Gaza",
+    date: "2005-08-15",
+  },
 
-// renderDev(<Timeline events={mock} />)
+];
+
+const stdate = new Date('1948-05-13');
+const endate = new Date('2005-08-15');
+const timespan = differenceInDays(endate, stdate);
+
+renderDev(
+  <Timeline
+    events={mock}
+    startDate={stdate}
+    endDate={endate}
+    eventCount={10}
+    timeSpan={timespan}
+    daysPerEventDsp={Math.ceil(timespan/10)}
+    className={"w-full h-full"}
+  />
+);
--- lib/src/components/timeline/Timeline.tsx
+++ lib/src/components/timeline/Timeline.tsx
@@ -1,56 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { differenceInDays } from "date-fns";
-import { EventDsp } from 'components/event/EventDsp';
-import { EventType } from 'types/interfaces';
-import { cn } from 'shared/methods';
+import { EventDsp } from "components/event/EventDsp";
+import { EventType } from "types/interfaces";
+import { cn } from "shared/methods";
 
 interface TimelineType {
-    events: EventType[];
-    startDate: Date;
-    endDate: Date;
-    eventCount: number;
-    timeSpan: number;
-    daysPerEventDsp: number;
-    className?: string;
+  events: EventType[];
+  startDate: Date;
+  endDate: Date;
+  eventCount: number;
+  timeSpan: number;
+  daysPerEventDsp: number;
+  className?: string;
 }
 
 export function Timeline({
-    events,
-    startDate,
-    endDate,
-    eventCount,
-    timeSpan,
-    daysPerEventDsp,
-    className
+  events,
+  startDate,
+  endDate,
+  eventCount,
+  timeSpan,
+  daysPerEventDsp,
+  className,
 }: TimelineType) {
+  const [items, setItems] = useState<EventType[][]>([]);
 
-    const [items, setItems] = useState<EventType[][]>([]);
-
-    useEffect(() => {
-        if (eventCount) {
-            const dates: any = Array.from({ length: eventCount }, () => []);
-            for (let i in events) {
-                const date = new Date(events[i].date + '');
-                if (date >= startDate && date <= endDate) {
-                    const diff = differenceInDays(date, startDate);
-                    const fin = Math.floor(diff / daysPerEventDsp);
-                    dates[fin].push(events[i]);
-                    dates[fin] = dates[fin].sort(
-                        (a: EventType, b: EventType) =>
-                            differenceInDays(b.date + '', a.date + '')
-                    );
-                }
-            }
-            setItems(dates);
+  useEffect(() => {
+    if (eventCount) {
+      const dates: any = Array.from({ length: eventCount }, () => []);
+      for (let i in events) {
+        const date = new Date(events[i].date + "");
+        if (date >= startDate && date <= endDate) {
+          const diff = differenceInDays(date, startDate);
+          const fin = Math.floor(diff / daysPerEventDsp);
+          dates[fin].push(events[i]);
+          dates[fin] = dates[fin].sort((a: EventType, b: EventType) =>
+            differenceInDays(b.date + "", a.date + "")
+          );
         }
-    }, [eventCount, startDate, endDate, timeSpan])
+      }
+      setItems(dates);
+    }
+  }, [eventCount, startDate, endDate, timeSpan]);
 
-    return <div className={cn(className, 'flex flex-col [&>*:not(:last-child)]:border-b')}>
-        {items.map((e, i) =>
-            <EventDsp
-                key={`key_timeline_events_${i}`}
-                className='w-full h-full'
-                events={e}
-            />)}
+  return (
+    <div
+      className={cn(className, "flex flex-col [&>*:not(:last-child)]:border-b")}
+    >
+      {items.map((e, i) => (
+        <EventDsp
+          key={`key_timeline_events_${i}`}
+          className="w-full h-full"
+          events={e}
+        />
+      ))}
     </div>
+  );
 }
